a060522d69cf6d827c8d61410ddcffb3cdf4f501
Cyoxite <ander.polanski@gmail.com>
2021-09-28 14:37:49 +0200

    init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="My avatar" src="../assets/Avatar.png" width="120" height="120">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
