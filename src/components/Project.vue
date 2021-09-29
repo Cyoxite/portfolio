b0816a1d87f8de14b5606007ea61f3c0caa8d196
Cyoxite <ander.polanski@gmail.com>
2021-09-29 17:26:58 +0200

    in project components added technology list and miniature gallery

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,16 +2,43 @@
     
 
 <div class="completed" v-bind:key="completed.id" >
-<h3> {{completed.name}}<button @click='toggle = !toggle'>Show more</button></h3>
-<div class="Show" v-show='!toggle'>
-<div class="gallery">
-<img class="miniature" v-for="photo in completed.photo"
-  :key="photo.id" :src="photo.link">
-  
-  </div>
-<div class="description">{{completed.description}}</div>
-<div class="technology">Technology</div>
-</div>
+<h2> {{completed.name}} <button @click=getbuttontext>{{buttontext}}</button></h2>
+        <div class="Show" v-show='!toggle'>
+
+            <!-- GALLERY FRAGMENT -->
+        <div class="gallery">
+        <img class="miniature" v-for="photo in completed.photo"
+        :key="photo.link" :src="photo.link">
+        </div>
+
+            <!-- DESCRIPTION FRAGMENT -->
+        <div class="description">{{completed.description}}</div>
+
+            <!-- LIST OF FEATURES FRAGMENT -->
+            <div class="features" >
+                <ol> <h4>Features</h4>
+                <li v-for="feature in completed.feature"
+        :key="feature.featurenm"
+                
+                >{{feature.featurenm}}</li>
+                </ol>
+            
+        </div>
+
+             <!-- TECHNOLOGY FRAGMENT -->
+        <div class="technology">Technology
+            <ol>
+                <li
+                v-for="technology in completed.technology"
+                :key="technology"
+                >
+                {{technology}}
+                </li>
+            </ol>
+
+
+        </div>
+        </div>
 </div>
 
 </template>
@@ -20,13 +47,31 @@
 export default {
     data () {
    return {
-     toggle: true
+     toggle: true,
+     buttontext: 'show more',
    }
     },
     props: 
-    ['completed']
+    ['completed'],
+    methods: {
+        getbuttontext: function (){
+            this.toggle = !this.toggle;
+            if (this.toggle == false){
+                this.buttontext = 'hide'
+                return
+            } else {
+                this.buttontext = 'show more'
+                return
+            }
+            }
 
-}
+        
+            
+        }
+
+    }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
